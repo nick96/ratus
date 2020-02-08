117a863dd6950354eb8b1099a0ae1c3bcb6828c7
Nick Spain <nicholas.spain96@gmail.com>
2020-02-08 12:44:25 +1100

    Use BinaryOpType and UnaryOpType enums for mapping binary/unary operations to their functions

--- ratus/execer.py
+++ ratus/execer.py
@@ -1,7 +1,15 @@
 import operator
 from typing import Any, Callable, Dict, Optional
 
-from ratus.parse import BinaryOp, Expression, Function, Literal, UnaryOp
+from ratus.parse import (
+    BinaryOp,
+    BinaryOpType,
+    Expression,
+    Function,
+    Literal,
+    UnaryOp,
+    UnaryOpType,
+)
 
 
 class ExecutorError(Exception):
@@ -14,47 +22,48 @@ class Executor:
     def __init__(
         self,
         functions: Optional[Dict[str, Callable[..., Any]]] = None,
-        binary_ops: Optional[Dict[str, Callable[[Any, Any], Any]]] = None,
-        unary_ops: Optional[Dict[str, Callable[[Any], Any]]] = None,
+        binary_ops: Optional[Dict[BinaryOpType, Callable[[Any, Any], Any]]] = None,
+        unary_ops: Optional[Dict[UnaryOpType, Callable[[Any], Any]]] = None,
     ) -> None:
         """
         Instantiate an Executor object.
 
         This constructor provides three arguments to control behavior.
 
-        `functions` allows us to extend the callable functions in expression. By default
-        only `if` is provided but more can be added and the default definition of `if`
-        (`lambda c, s, f: s if c else f`) can be overridden simple by having an "if" key
-        in the `functions` dictionary.
+        `functions` allows us to extend the callable functions in expression. By
+        default only `if` is provided but more can be added and the default
+        definition of `if` (`lambda c, s, f: s if c else f`) can be overridden
+        simple by having an "if" key in the `functions` dictionary.
 
-        `binary_ops` allows us to extend the binary operations available. It is a
-        dictionary mapping variants of `ratus.parse.BinaryOpTypes` to a function with
-        two parameters and a single output.
+        `binary_ops` allows us to extend the binary operations available. It is
+        a dictionary mapping variants of `ratus.parse.BinaryOpTypes` to a
+        function with two parameters and a single output.
 
         `unary_ops` allows us to extend the unary operations available. It is a
-        dictionary mapping variants of `ratus.parse.UnaryOpTypes` to a function with one
-        parameter and one outputs.
+        dictionary mapping variants of `ratus.parse.UnaryOpTypes` to a function
+        with one parameter and one outputs.
+
         """
-        self.binary_ops: Dict[str, Callable[[Any, Any], Any]] = {
-            "+": operator.add,
-            "-": operator.sub,
-            "*": operator.mul,
-            "/": operator.truediv,
-            ">": operator.gt,
-            ">=": operator.ge,
-            "<": operator.lt,
-            "<=": operator.le,
-            "and": operator.and_,
-            "or": operator.or_,
-            "=": operator.eq,
-            "!=": operator.ne,
+        self.binary_ops: Dict[BinaryOpType, Callable[[Any, Any], Any]] = {
+            BinaryOpType.ADDITION: operator.add,
+            BinaryOpType.SUBTRACTION: operator.sub,
+            BinaryOpType.MULTIPLICATION: operator.mul,
+            BinaryOpType.DIVISION: operator.truediv,
+            BinaryOpType.GREATER: operator.gt,
+            BinaryOpType.GREATER_EQUAL: operator.ge,
+            BinaryOpType.LESS: operator.lt,
+            BinaryOpType.LESS_EQUAL: operator.le,
+            BinaryOpType.AND: operator.and_,
+            BinaryOpType.OR: operator.or_,
+            BinaryOpType.EQUAL: operator.eq,
+            BinaryOpType.NOT_EQUAL: operator.ne,
         }
         if binary_ops is not None:
             self.binary_ops.update(binary_ops)
 
-        self.unary_ops: Dict[str, Callable[[Any], Any]] = {
-            "!": operator.not_,
-            "-": operator.neg,
+        self.unary_ops: Dict[UnaryOpType, Callable[[Any], Any]] = {
+            UnaryOpType.NOT: operator.not_,
+            UnaryOpType.NEGATIVE: operator.neg,
         }
         if unary_ops is not None:
             self.unary_ops.update(unary_ops)
@@ -70,11 +79,11 @@ class Executor:
         if isinstance(expression, BinaryOp):
             left = self.execute(expression.left)
             right = self.execute(expression.right)
-            binary_op = self.binary_ops[expression.op_type.value]
+            binary_op = self.binary_ops[expression.op_type]
             return binary_op(left, right)
         if isinstance(expression, UnaryOp):
             operand = self.execute(expression.operand)
-            unary_op = self.unary_ops[expression.op_type.value]
+            unary_op = self.unary_ops[expression.op_type]
             return unary_op(operand)
         if isinstance(expression, Function):
             function = self.functions.get(expression.name)
